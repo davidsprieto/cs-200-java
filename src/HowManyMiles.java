1e3e88da4aa36a4c25bfbd491cf6879b225e6d73
David Prieto <david.s.prieto1@gmail.com>
2022-08-31 17:19:41 -0500

    Completed Workshop Problem #2

---
+++ src/HowManyMiles.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class HowManyMiles {
+
+    public static void main(String[] args) {
+        Scanner kbd = new Scanner(System.in);
+
+        int miles;
+        System.out.print("Enter a distance in miles: ");
+        miles = kbd.nextInt();
+
+        int milesInInches = miles * 5280 * 12;
+        System.out.println("The distance in inches is: " + milesInInches);
+
+        int inchesToFeet = ((milesInInches + 5) * 2) / 12;
+        System.out.println("Your new distance is " + inchesToFeet + " ft." + " and 5 in.");
+
+        double newMiles = (double) inchesToFeet / 5280;
+        System.out.println("The total miles is now " + newMiles);
+    }
+
+}
