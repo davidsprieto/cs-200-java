5b02ed060678495c3968bfa2a8088cff7032a01b
David Prieto <david.s.prieto1@gmail.com>
2022-09-02 18:15:16 -0500

    Completed CS200 - Workshop Assignment

---
+++ src/Tracing.java
@@ -0,0 +1,30 @@
+public class Tracing {
+
+    public static void main(String[] args) {
+        tracing4();
+        tracing5();
+    }
+
+    public static void tracing4() {
+        int a = 2, b = 7, m = 6, n = 3, o = 14;
+        double d = 2.0;
+        String c = "Fall";
+
+        System.out.println(a + b - m + n);
+        System.out.println(n - a + "" + (o - b));
+        System.out.println(c + d + (b - m) + (o - b));
+        System.out.println(a * b % a + m / n);
+        System.out.println(b / a + c + m % n);
+    }
+
+    public static void tracing5() {
+        int u = 2, v = 3, w = 5, x = 7, y = 11;
+        String s = "Spring";
+
+        System.out.println(u + v * w + x);
+        System.out.println(u + y % v * w + x);
+        System.out.println(u / v * w);
+        System.out.println(v + s + x / v);
+    }
+
+}
