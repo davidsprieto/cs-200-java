ec896a2d021d6ac539bcc338ddea02c8dbf73c0e
David Prieto <david.s.prieto1@gmail.com>
2022-09-23 22:04:12 -0500

    Completed week 4 wrkshop assignment

---
+++ src/WeekFour/Distance.java
@@ -0,0 +1,28 @@
+package WeekFour;
+
+import java.util.Scanner;
+
+public class Distance {
+
+    public static void main(String[] args) {
+
+        Scanner kbd = new Scanner(System.in);
+
+        System.out.print("Enter x1: ");
+        double x1 = kbd.nextInt();
+
+        System.out.print("Enter y1: ");
+        double y1 = kbd.nextInt();
+
+        System.out.print("Enter x2: ");
+        double x2 = kbd.nextInt();
+
+        System.out.print("Enter y2: ");
+        double y2 = kbd.nextInt();
+
+        double distance = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+
+        System.out.println("The distance is: " + distance + ".");
+    }
+
+}
---
+++ src/WeekFour/SplitWord.java
@@ -0,0 +1,25 @@
+package WeekFour;
+
+import java.util.Scanner;
+
+public class SplitWord {
+
+    public static void main(String[] args) {
+
+        Scanner kbd = new Scanner(System.in);
+
+        System.out.print("Enter a word: ");
+        String word = kbd.nextLine();
+
+        int wordLength = word.length();
+        char firstLetter = word.charAt(0);
+        char midLetter = word.charAt(wordLength / 2);
+        char lastLetter = word.charAt(wordLength - 1);
+
+        System.out.println("The length of \"" + word + "\" is " + wordLength);
+        System.out.println("The first letter is \"" + firstLetter + "\"");
+        System.out.println("The middle letter is \"" + midLetter + "\"");
+        System.out.println("The last letter is \"" + lastLetter + "\"" );
+    }
+
+}
