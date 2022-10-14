cd9a24f363f4d36845cad87797315290c759a9a5
David Prieto <david.s.prieto1@gmail.com>
2022-10-14 11:09:23 -0500

    Completed HW Problem #1

---
+++ src/WeekSeven/Reverse.java
@@ -0,0 +1,34 @@
+// CS200
+// Fall, 2022
+// David Prieto
+// Instructor: Y. Gutstein
+// HW #1: Arrays
+// Due: 10/14/2022
+// Reverse.java
+
+package WeekSeven;
+
+import java.util.Scanner;
+
+public class Reverse {
+
+    public static void main(String[] args) {
+
+        Scanner kbd = new Scanner(System.in);
+
+        System.out.print("Please enter 10 integers: ");
+
+        int[] array = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            array[i] = kbd.nextInt();
+        }
+
+        System.out.print("The reverse is: ");
+        for (int i = 9; i >= 0; i--) {
+            System.out.print(array[i] + " ");
+        }
+
+    }
+
+}
