725a484919a5155fd4db263435972b5d87b28391
David Prieto <david.s.prieto1@gmail.com>
2022-11-17 18:57:32 -0600

    Wrapper Classes WS

---
+++ src/WeekTwelve/WorkshopTwelve/WrapperClasses.java
@@ -0,0 +1,9 @@
+package WeekTwelve.WorkshopTwelve;
+
+public class WrapperClasses {
+    public static void main(String[] args) {
+        Double x = 3.5;
+        System.out.println(x.intValue());
+        System.out.println(x.compareTo(4.5));
+    }
+}
